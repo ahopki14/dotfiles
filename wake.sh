a104df73279f2f138017d8e3fe9edf2501f8728a
Alex Hopkins <ahopkins@dexterbiometry.com>
2026-08-28 20:36:56 +0000

    added WoL script for 3 servers

---
+++ wake.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+case "$1" in
+  sourdough )
+        mac='18:66:DA:4E:30:D5'
+    ;;
+  rye )
+        mac='18:66:DA:4E:38:47'
+    ;;
+  hardtack )
+        mac='D0:50:99:C1:EB:D1'
+    ;;
+
+  *)
+    echo "Usage: $0 {sourdough|rye|hardtack}"
+    exit
+    ;;
+esac
+
+echo "wakeonlan -i 192.168.50.255 $mac"
